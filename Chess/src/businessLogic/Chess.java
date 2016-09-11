5addc64b80e76991a3e0203227a906c408504aaa
USUARIO <USUARIO@DESKTOP-FRER0OG>
2016-09-11 13:08:39 -0500

    main clear

--- Chess/src/businessLogic/Chess.java
+++ Chess/src/businessLogic/Chess.java
@@ -6,13 +6,6 @@ import ui.UI;
 public class Chess {
 
     public static void main(String[] args) {
-        Player player1=new Player();
-        Player player2=new Player();
-        player1=UI.createPlayer(player1,true);
-        player2=UI.createPlayer(player2,false);
-        
-        
-        
         
     }
     
