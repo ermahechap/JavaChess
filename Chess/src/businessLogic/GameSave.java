0fe782051f072fda3c1ef85f8960254bca2e3696
edwmapa <edwmapa@gmail.com>
2016-10-13 13:53:16 -0500

    new clases

---
+++ Chess/src/businessLogic/GameSave.java
@@ -0,0 +1,13 @@
+package businessLogic;
+
+import data.*;
+import java.io;
+
+public class GameSave {
+    protected static void loadGame(){
+        
+    }
+    protected static void saveGame(Board  board, Player[] player){
+        
+    }
+}
---
+++ Chess/src/ui/GUI.java
@@ -0,0 +1,5 @@
+package ui;
+
+public class GUI {
+    //here goes magic
+}
