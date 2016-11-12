dc671a1d2f5b9c049d07f3dafd6ca8315a8f9a26
edwmapa <edwmapa@gmail.com>
2016-11-12 12:15:43 -0500

    Swing UI rebooted, Jframe creation

--- Chess/src/ui/UISwing.java
+++ Chess/src/ui/UISwing.java
@@ -1,36 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ui;
 
 import data.Board;
 import data.Piece;
 import data.Player;
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.JFileChooser;
-import java.io.File; 
-import javax.swing.JOptionPane;
 
+/**
+ *
+ * @author USUARIO
+ */
 public class UISwing extends javax.swing.JFrame implements UI{
-    
-    private static String messages[]= new String[]{"Opcion no listada","No hay pieza en la posición inicial"
-            ,"Se espera valor numerico","Ingrese una coordenada valida, es decir letra y número"
-            ,"Coordenedas Incorrectas o fuera del limite","La dirección es incorrecta, debe ingresar solo la dirección de LA CARPETA"
-            ,"Coordenadas iguales, intente otra vez","Movimiento no valido"};
-    
+        private boolean menuBtnPressed=false;
+        private int menuSelection=1;
     public UISwing() {
         try {
             initComponents();
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             SwingUtilities.updateComponentTreeUI(this);
-            this.setIconImage(new ImageIcon(getClass().getResource("/resources/gameIcon.png")).getImage());
-            this.setTitle("JavaChess");
+            ImageIcon icon= new ImageIcon("/resources/gameIcon.png");
+            this.setIconImage(icon.getImage());
             this.pack();
             this.setVisible(true);
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UISwing.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(UISwing.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(UISwing.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
             Logger.getLogger(UISwing.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -44,31 +54,120 @@ public class UISwing extends javax.swing.JFrame implements UI{
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        jPopupMenu1 = new javax.swing.JPopupMenu();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        jButtonNewGame = new javax.swing.JButton();
+        jButtonLoad = new javax.swing.JButton();
+        jButtonExit = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("JavaChess");
         setResizable(false);
-        getContentPane().setLayout(new java.awt.CardLayout());
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 833, Short.MAX_VALUE)
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/Title.png"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addContainerGap(23, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 456, Short.MAX_VALUE)
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        getContentPane().add(jPanel1, "card2");
+        getContentPane().add(jPanel2, java.awt.BorderLayout.PAGE_START);
+
+        jButtonNewGame.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/horse.png"))); // NOI18N
+        jButtonNewGame.setText("Nuevo Juego");
+        jButtonNewGame.setAlignmentX(0.5F);
+        jButtonNewGame.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonNewGameActionPerformed(evt);
+            }
+        });
+
+        jButtonLoad.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/folder.png"))); // NOI18N
+        jButtonLoad.setText("Cargar Juego");
+        jButtonLoad.setAlignmentX(0.5F);
+        jButtonLoad.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonLoadActionPerformed(evt);
+            }
+        });
+
+        jButtonExit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/exit.png"))); // NOI18N
+        jButtonExit.setText("Salir");
+        jButtonExit.setAlignmentX(0.5F);
+        jButtonExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonExitActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButtonExit, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButtonNewGame, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButtonLoad, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 571, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButtonNewGame, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButtonLoad)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButtonExit)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        getContentPane().add(jPanel3, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButtonNewGameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNewGameActionPerformed
+        menuBtnPressed=true;
+        menuSelection=1;
+    }//GEN-LAST:event_jButtonNewGameActionPerformed
+
+    private void jButtonLoadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonLoadActionPerformed
+        menuBtnPressed=true;
+        menuSelection=2;
+    }//GEN-LAST:event_jButtonLoadActionPerformed
+
+    private void jButtonExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonExitActionPerformed
+        this.setVisible(false);
+        this.dispose();
+        menuBtnPressed=true;
+        menuSelection=3;
+    }//GEN-LAST:event_jButtonExitActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton jButtonExit;
+    private javax.swing.JButton jButtonLoad;
+    private javax.swing.JButton jButtonNewGame;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPopupMenu jPopupMenu1;
     // End of variables declaration//GEN-END:variables
 
     @Override
@@ -125,38 +224,23 @@ public class UISwing extends javax.swing.JFrame implements UI{
     public void printBoard(Board board) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    @Override
-    
-    public String readName(String col) {
-        return "nothing";
-    }
 
     @Override
-    public Player[] readPlayers() {
-        NewGamePanel newGameP=new NewGamePanel();
-        this.getContentPane().removeAll();
-        this.getContentPane().add(newGameP);
-        this.revalidate();
-        this.repaint();
-        this.pack();
-        return newGameP.generatePlayers();
+    public String readName(String col) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @Override
     public void welcome() {
     }
 
     @Override
     public int menu() {
-        MenuPanel mPanel=new MenuPanel();
-        
-        this.getContentPane().removeAll();
-        this.getContentPane().add(mPanel);
-        this.revalidate();
-        this.repaint();
-        this.pack();
-        int opt=mPanel.getChosen();//get choosen from pane
-        return opt;
+        do {
+            pause();
+        } while (!menuBtnPressed);
+        menuBtnPressed=false;
+        return menuSelection;
     }
 
     @Override
@@ -206,16 +290,7 @@ public class UISwing extends javax.swing.JFrame implements UI{
 
     @Override
     public String loadGameRequest() {
-        JFileChooser fileChoser= new JFileChooser(new File(System.getProperty("user.home")));
-        fileChoser.setMultiSelectionEnabled(false);
-        fileChoser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        int res = fileChoser.showOpenDialog(this);
-        if(res==JFileChooser.APPROVE_OPTION){
-            String path= fileChoser.getSelectedFile().getAbsolutePath();
-            return path;
-        }else{
-            return "";
-        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
@@ -230,12 +305,36 @@ public class UISwing extends javax.swing.JFrame implements UI{
 
     @Override
     public void onLoadFailure() {
-        JOptionPane.showMessageDialog(this, "No se puede cargar la partida\nnueva partida iniciada", "Fallo en carga de archivo!!", JOptionPane.ERROR_MESSAGE);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void onLoadSuceed() {
-        JOptionPane.showMessageDialog(this, "Carga realizada correctamente", "Carga correcta!!", JOptionPane.INFORMATION_MESSAGE);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    @Override
+    public Player[] readPlayers() {
+        this.setVisible(false);
+        PlayerRegisterMenu register=new PlayerRegisterMenu();
+        register.setLocationRelativeTo(this);
+        do {
+            pause();
+        } while (!register.isBtnPressed());
+        Player player[]={new Player(register.getPlayerWhite(), true)
+                ,new Player(register.getPlayerBlack(), false)};
+        register.setVisible(false);
+        register=null;
+        
+        return player;
+    }
+    
+    public void pause(){
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(UISwing.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
 }
